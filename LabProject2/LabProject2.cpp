671e607db1da46599000ba44b36373ba1be4d4e6
Yevhen <evgenzuzin35@gmail.com>
2025-11-21 22:24:46 +0200

    greeting added

--- LabProject2/LabProject2.cpp
+++ LabProject2/LabProject2.cpp
@@ -1,10 +1,17 @@
 ﻿#define _USE_MATH_DEFINES 
-
 #include <iostream>
 #include <cmath>
 #include <format>
 
 using namespace std;
+
+void greeting() {
+	/*
+	* Prints the author's name and the program's purpose.
+	*/
+	cout << "The author of this program is Ziuzin Yevhen.\n";
+	cout << "This program calculates the expression from variant by given x. Variant 105.\n";
+}
 
 int main()
 {
