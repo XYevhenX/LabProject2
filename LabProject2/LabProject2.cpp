edb7fc1cd8898b579776883f423b5da893db0799
Yevhen <evgenzuzin35@gmail.com>
2025-11-21 22:29:55 +0200

    input function and try construction added

--- LabProject2/LabProject2.cpp
+++ LabProject2/LabProject2.cpp
@@ -12,8 +12,29 @@ void greeting() {
 	cout << "The author of this program is Ziuzin Yevhen.\n";
 	cout << "This program calculates the expression from variant by given x. Variant 105.\n";
 }
-
+double input() {
+	/*
+	* Prompts the user to enter a double value for 'x'.
+	* Throws invalid_argument if the input stream fails.
+	* Returns the double value entered by the user.
+	*/
+	double x;
+	cout << "Please, enter x from domain (): ";
+	cin >> x;
+	if (cin.fail()) {
+		throw runtime_error("wrong input");
+	}
+	cout << endl;
+	return x;
+}
 int main()
 {
-    
+    try
+    {
+        
+    }
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	return 0;
 }
